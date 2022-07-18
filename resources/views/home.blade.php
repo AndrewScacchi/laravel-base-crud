1cce80b7408df3a5641a0f2047f85aa76612962f
andrea scacchi <andrea.scacchi@live.com>
2022-07-18 15:34:47 +0200

    Initial Commit. Basic Folder and file structure, basic view and includes structure, flavour scss Added db data on .env

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('template.base')
+
+@section('pageTitle', 'Comics')
+
+@section('pageMain')
+<main>
+    <h1>This is the Home</h1>
+</main>
+@endsection
---
+++ resources/views/includes/_header.blade.php
@@ -0,0 +1,3 @@
+<header>
+    <h3>This is the Header</h3>
+</header>
